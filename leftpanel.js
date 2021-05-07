4e89b6be63e802c78b516d662668598259f64da5
DanilovAndrey <52465053+DanilovAndrey@users.noreply.github.com>
2021-05-07 13:11:42 +0300

    Initial commit

---
+++ leftpanel.js
@@ -0,0 +1,9 @@
+<div className='item1'>
+    <div className='grid-container3'>
+        <button className='leftbutton1' type="button" onClick="alert('Hello world!')">Click Me!</button>
+        <button className='leftbutton2' type="button" onClick="alert('Hello world!')">Click Me!</button>
+        <button className='leftbutton3' type="button" onClick="alert('Hello world!')">Click Me!</button>
+        <button className='leftbutton4' type="button" onClick="alert('Hello world!')">Click Me!</button>
+        <button className='leftbutton5' type="button" onClick="alert('Hello world!')">Click Me!</button>
+    </div>
+</div>
